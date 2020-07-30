6b2414717e714c9b7e227b3a926f696684e7a678
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-30 13:24:56 -0500

    data types

--- 2-JS-basics/final/script.js
+++ 2-JS-basics/final/script.js
@@ -1,3 +1,25 @@
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+
+
+
+
+
+
+
 /*****************************
 * Variables and data types
 */

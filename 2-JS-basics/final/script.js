d9d17f43a0899bc68e8e58121bc796240bb81262
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-30 13:47:45 -0500

    type mutation and coercion

--- 2-JS-basics/final/script.js
+++ 2-JS-basics/final/script.js
@@ -1,3 +1,4 @@
+//Variables and data types
 var firstName = 'John';
 console.log(firstName);
 
@@ -13,55 +14,23 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
+// Variable naming rules cannot
+// var _3years = 3;
+// var johnMark = 'John and MArk';
+// var if = 23;
 
 
 
-
-
-
-
-/*****************************
-* Variables and data types
-*/
-/*
-var firstName = 'John';
-console.log(firstName);
-
-var lastName = 'Smith';
+// Variable mutation and type coercion
+var firstName= 'John';
 var age = 28;
-
-var fullAge = true;
-console.log(fullAge);
-
-var job;
-console.log(job);
-
-job = 'Teacher';
-console.log(job);
-
-// Variable naming rules
-var _3years = 3;
-var johnMark = 'John and MArk';
-var if = 23;
-*/
-
-
-
-/*****************************
-* Variable mutation and type coercion
-*/
-/*
-var firstName = 'John';
-var age = 28;
-
-// Type coercion
 console.log(firstName + ' ' + age);
 
 var job, isMarried;
 job = 'teacher';
 isMarried = false;
+console.log(firstName + ' is a ' + age + ' year old ' + job + ' .Is he married? ' + isMarried);
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
 // Variable mutation
 age = 'twenty eight';
@@ -71,7 +40,7 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
-*/
+
 
 
 

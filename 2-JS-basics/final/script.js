b643d1031fc8fae2622b570de963cb5bad39012b
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-30 20:27:51 -0500

    functions

--- 2-JS-basics/final/script.js
+++ 2-JS-basics/final/script.js
@@ -222,51 +222,28 @@ switch (true) {
         console.log(firstName + ' is a man.');
 }
 
-
-
-
-/*****************************
-* Truthy and Falsy values and equality operators
-*/
-/*
-// falsy values: undefined, null, 0, '', NaN
-// truthy values: NOT falsy values
-
-var height;
-
-height = 23;
-
-if (height || height === 0) {
-    console.log('Variable is defined');
-} else {
-    console.log('Variable has NOT been defined');
-}
-
 // Equality operators
-if (height === '23') {
-    console.log('The == operator does type coercion!');
-}
-*/
+// == means its equal to different data types eg: string '23' can equal number 23
+// if (height === '23') {
+//     console.log('The == operator does type coercion!');
+// }
 
 
 
-/*****************************
-* CODING CHALLENGE 2
-*/
 
-/*
-John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+// CODING CHALLENGE 2
 
-1. Calculate the average score for each team
-2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
-3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
 
-4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
-5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
 
-GOOD LUCK 😀
-*/
-/*
+// John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+// 1. Calculate the average score for each team
+// 2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+// 3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+// 4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+// 5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
 var scoreJohn = (189 + 120 + 103) / 3;
 var scoreMike = (129 + 94 + 123) / 3;
 var scoreMary = (97 + 134 + 105) / 3;
@@ -290,7 +267,48 @@ if (scoreJohn > scoreMike) {
 } else {
     console.log('There is a draw');
 }
-*/
+
+
+
+//Functions
+//argument in paranthesis
+
+function calculateAge (birthYear) {
+    return 2018 - birthYear;
+}
+    
+    var ageJohn = calculateAge(1990);
+    var ageMike = calculateAge(1948);
+    var ageJane = calculateAge(1969);
+    console.log(ageJohn, ageMike, ageJane);
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    if (retirement > 0 ) {
+        console.log(firstName + ' retires in ' + retirement + ' years.');
+    }else{
+        console.log(firstName + ' retires in ' + ' is already retired');
+    }
+    
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

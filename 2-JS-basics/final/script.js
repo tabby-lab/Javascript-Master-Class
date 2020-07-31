212c86fdfe44a22f4f33d220fdee4dc4740a282a
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-30 21:30:28 -0500

    arrays and dif data types

--- 2-JS-basics/final/script.js
+++ 2-JS-basics/final/script.js
@@ -297,58 +297,8 @@ yearsUntilRetirement(1990, 'John');
 yearsUntilRetirement(1948, 'Mike');
 yearsUntilRetirement(1969, 'Jane');
 
+//Function Statements and Expressions
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*****************************
-* Functions
-*/
-/*
-function calculateAge(birthYear) {
-    return 2018 - birthYear;
-}
-
-var ageJohn = calculateAge(1990);
-var ageMike = calculateAge(1948);
-var ageJane = calculateAge(1969);
-console.log(ageJohn, ageMike, ageJane);
-
-
-function yearsUntilRetirement(year, firstName) {
-    var age = calculateAge(year);
-    var retirement = 65 - age;
-
-    if (retirement > 0) {
-        console.log(firstName + ' retires in ' + retirement + ' years.');
-    } else {
-        console.log(firstName + ' is already retired.')
-    }
-
-}
-
-yearsUntilRetirement(1990, 'John');
-yearsUntilRetirement(1948, 'Mike');
-yearsUntilRetirement(1969, 'Jane');
-*/
-
-
-
-/*****************************
-* Function Statements and Expressions
-*/
-/*
 // Function declaration
 // function whatDoYouDo(job, firstName) {}
 
@@ -369,20 +319,13 @@ var whatDoYouDo = function(job, firstName) {
 console.log(whatDoYouDo('teacher', 'John'));
 console.log(whatDoYouDo('designer', 'Jane'));
 console.log(whatDoYouDo('retired', 'Mark'));
-*/
 
+// Arrays
 
-
-/*****************************
-* Arrays
-*/
-/*
-// Initialize new array
 var names = ['John', 'Mark', 'Jane'];
-var years = new Array(1990, 1969, 1948);
-
-console.log(names[2]);
+var years = new Array(1990,1969,1948);
 console.log(names.length);
+console.log(names[2]);
 
 // Mutate array data
 names[1] = 'Ben';
@@ -390,22 +333,33 @@ names[names.length] = 'Mary';
 console.log(names);
 
 // Different data types
-var john = ['John', 'Smith', 1990, 'designer', false];
-
+var john = ['John', 'Smith', 1990, 'teacher',false];
 john.push('blue');
 john.unshift('Mr.');
 console.log(john);
 
-john.pop();
-john.pop();
+john.pop()
 john.shift();
 console.log(john);
 
-console.log(john.indexOf(23));
-
-var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+var isDesigner = john.indexOf('designer') === -1 ?
+'John is Not a designer' : 'John is a designer';
 console.log(isDesigner);
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

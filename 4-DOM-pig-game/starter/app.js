3a2f9ddff67c804167d508ebe783df31aeef64ab
Tabby Garcia <tabby.garcia1@gmail.com>
2020-08-04 21:11:58 -0500

    check to see if a player wins

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -81,6 +81,8 @@ document.querySelector('#score-' + activePlayer).textContent = scores[activePlay
 if (scores[activePlayer] >= 20) {
     document.querySelector('#name-' + activePlayer).textContent = 'WINNER';
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 } else {
     nextPlayer();
 }
